b307799bcac3399c0211183a88780adc7819fe2e
draganculibrk9 <d.culibrk@vegait.rs>
2024-03-08 10:07:36 +0100

    feat: Complete exercise 1/11

--- 1/11/log-output/index.js
+++ 1/11/log-output/index.js
@@ -1,19 +1,21 @@
 import {generateHash} from 'random-hash';
 import fs from 'fs/promises';
 
-const PINGPONG_FILE = '/usr/src/app/files/pingpong.txt';
+const PONG_FILE = '/usr/src/app/files/pong.txt';
 
-const readPingPongsFromFile = async () => {
-    return (await fs.readFile(PINGPONG_FILE)).toString();
+const readPingsFromFile = async () => {
+    return (await fs.readFile(PONG_FILE)).toString();
 }
 
 const displayGeneratedStringOnStartup = async () => {
     const hash = generateHash();
 
     const displayString = async () => {
-        const timestamp = await readTimestampFromFile();
+        const timestamp = new Date().toJSON();
+        const pings = await readPingsFromFile();
         
         console.log(`${timestamp}: ${hash}`);
+        console.log(`Ping / Pongs: ${pings}`)
         setTimeout(displayString, 5000);
     }
     await displayString();
--- 1/11/ping-pong/index.js
+++ 1/11/ping-pong/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const fs = require('fs/promises');
 
 const app = express();
 const port = 3000;
 
+const PONG_FILE = '/usr/src/app/files/pong.txt';
+
+const writeCount = async (count) => {
+    fs.writeFile(PONG_FILE, count.toString());
+}
+
 let counter = 0;
 
-app.get('/pingpong', (_, res) => {
+app.get('/pingpong', async (_, res) => {
     res.status(200).send(`pong ${counter}`);
+    await writeCount(counter);
     counter++;
 })
 
