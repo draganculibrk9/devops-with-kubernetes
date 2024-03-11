93baa311f17a8b057f30d3bae2f226c37d100cf9
draganculibrk9 <d.culibrk@vegait.rs>
2024-03-11 10:01:17 +0100

    feat: Complete exercise 2/01

---
+++ 2/01/ping-pong/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+let counter = 0;
+
+app.get('/pingpong', async (_, res) => {
+    res.status(200).send({ count: counter });
+    counter++;
+})
+
+app.listen(port, () => {
+    console.log(`Server started in port ${port}`)
+})
